cf117c1209e43385045148b4fa8153760d953986
Ashraful <ashraful.haq88@icloud.com>
2024-12-07 23:33:27 -0500

    4th commit

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,20 +3,24 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 import java.util.Properties;
-
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -28,29 +32,68 @@ public class BaseClass {
 	public Properties prop;
 
 	// setup()
-	@BeforeClass(groups = { "Master", "Regression", "Ddt","Sanity" })
+	@BeforeClass(groups = { "Master", "Regression", "Ddt", "Sanity" })
 	@Parameters({ "OS", "browser" })
-	public void setup(String od, String br) throws IOException {
+	public void setup(String od, String br) throws IOException, URISyntaxException {
+
 		// loading config properties file
 		FileReader file = new FileReader("./src/test/resources/config.properties");
 		prop = new Properties();
 		prop.load(file);
 		logger = LogManager.getLogger(this.getClass());
-		switch (br.toLowerCase()) {
-		case "chrome":
-			driver = new ChromeDriver();
-			break;
-		case "firfox":
-			driver = new FirefoxDriver();
-			break;
-		case "edge":
-			driver = new EdgeDriver();
-			break;
-		default:
-			System.out.println("This browser is not supported");
-			return;
-		}
+		
+        // Select execution environment
+		if (prop.getProperty("Execution_env").equalsIgnoreCase("remote")) {
 
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			
+			// OS
+			if (od.equalsIgnoreCase("Windows")) {
+				capabilities.setPlatform(Platform.WIN10);
+			} else if (od.equalsIgnoreCase("Mac")) {
+				capabilities.setPlatform(Platform.MAC);
+			} else if (od.equalsIgnoreCase("Linux")) {
+				capabilities.setPlatform(Platform.LINUX);
+			} else {
+				System.out.println("No matching OS");
+			}
+			
+			// browser
+			switch (br.toLowerCase()) {
+			case "chrome":
+				capabilities.setBrowserName("chrome");
+				break;
+			case "edge":
+				capabilities.setBrowserName("MicrosoftEdge");
+				break;
+			case "firefox":
+				capabilities.setBrowserName("firefox");
+				break;
+			default:
+				System.out.println("Invalid browser name..!");
+				return;
+			}
+
+			driver = new RemoteWebDriver(new URI("http://localhost:4444/wd/hub").toURL(), capabilities);
+
+		}
+		if (prop.getProperty("Execution_env").equalsIgnoreCase("local")) {
+
+			switch (br.toLowerCase()) {
+			case "chrome":
+				driver = new ChromeDriver();
+				break;
+			case "firfox":
+				driver = new FirefoxDriver();
+				break;
+			case "edge":
+				driver = new EdgeDriver();
+				break;
+			default:
+				System.out.println("This browser is not supported");
+				return;
+			}
+		}
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
@@ -60,7 +103,7 @@ public class BaseClass {
 	}
 
 	// tearDown()
-	@AfterClass(groups = { "Master", "Regression", "Ddt","Sanity" })
+	@AfterClass(groups = { "Master", "Regression", "Ddt", "Sanity" })
 	public void tearDown() {
 
 		driver.quit();
@@ -93,7 +136,7 @@ public class BaseClass {
 		String targetFilepath = System.getProperty("user.dir") + "/screenshots/" + name + "_" + timeStamp + ".png";
 		File targetFile = new File(targetFilepath);
 		src.renameTo(targetFile);
-		
+
 		return targetFilepath;
 	}
 
